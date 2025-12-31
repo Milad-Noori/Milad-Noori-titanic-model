dc0b2a2d5d22256d14e98245f82f699751ef1af7
milad <seyeadmiladnouri@gmail.com>
2025-12-31 23:40:18 +0330

    standardization

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from pandas import isnull
+from sklearn.preprocessing import StandardScaler
 
 data=pd.read_csv("titanic.csv")
 df=pd.DataFrame(data)
 print(df.isnull().sum())
 print(df.dtypes)
+
+ss=StandardScaler()
+x_rescale=ss.fit_transform(ss)
+print()
+
+
 
