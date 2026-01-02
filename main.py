089dfe4a0296639830784e31c2edb212af6b85d3
milad <seyeadmiladnouri@gmail.com>
2026-01-03 00:35:30 +0330

    find-null

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import matplotlib.pyplot as plt
 from pandas import isnull
 from sklearn.preprocessing import StandardScaler
 
-data=pd.read_csv("titanic.csv")
+data=pd.read_csv("titanic.csv",usecols=['PassengerId','Survived','Pclass'
+                                        ,'Name','Sex','Age',
+                                        'SibSp','Parch','Ticket','Fare','Cabin','Embarked'])
+
 df=pd.DataFrame(data)
+
+median=df['Age'].median()
+df['Age']= df['Age'].fillna(median)
+print(df)
 print(df.isnull().sum())
-print(df.dtypes)
-
-ss=StandardScaler()
-x_rescale=ss.fit_transform(ss)
-print()
-
-
-
