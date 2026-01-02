9cfbdaa7246f5125e437cf75151fb59157d8e586
milad <seyeadmiladnouri@gmail.com>
2026-01-03 01:42:20 +0330

    map-string

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from pandas import isnull
 from sklearn.preprocessing import StandardScaler
 
+
 data=pd.read_csv("titanic.csv",usecols=['PassengerId','Survived','Pclass'
                                         ,'Name','Sex','Age',
                                         'SibSp','Parch','Ticket','Fare','Cabin','Embarked'])
@@ -12,11 +13,26 @@ df=pd.DataFrame(data)
 ####
 # avg_age=df['Age'].mean()
 #####
+pd.set_option('display.max_row',None)
+pd.set_option('display.max_columns',None)
+
+# print(pd.get_option('display.max_row',None))
+# print(pd.get_option('display.max_columns',None))
+
 median=df['Age'].median()
 df['Age']= df['Age'].fillna(median)
 
 embarked_mode=df['Embarked'].mode()[0]
 df['Embarked'] = df['Embarked'].fillna(embarked_mode)
 df=df.drop('Cabin',axis=1)
-print(df.isnull().sum())
-# print(df.info())
+df=df.drop('Name',axis=1)
+df=df.drop('Ticket',axis=1)
+print(df['Embarked'].unique())
+print(df['Sex'].unique())
+df['Embarked']=df['Embarked'].map({'S':1,'C':2,'Q':3})
+df['Sex']=df['Sex'].map({'male':1,'female':2})
+
+# print(df.isnull().sum())
+# print(df)
+# print(df.corr(numeric_only=True))
+print(df.dtypes)
